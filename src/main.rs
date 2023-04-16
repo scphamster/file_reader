832a6faecb594b8def78f318ca3dad3d48329b07
scpha <scp.hamster@gmail.com>
2023-04-16 22:28:29 +0200

    more info to user

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> std::io::Result<()> {
 
     if args.len() < 2 {
         return
-            Result::Err(io::Error::new(io::ErrorKind::InvalidInput, "specify file to watch in invocation of this program!"));
+            Result::Err(io::Error::new(io::ErrorKind::InvalidInput, "specify file to watch in arguments! Example: file_reader C:\\tools\\etamps\\swadlwrapper.log"));
     }
 
     let file_name = args.get(1).ok_or(io::Error::new(io::ErrorKind::InvalidInput, "failed to get name of file specified")).unwrap();
@@ -25,6 +25,11 @@ fn main() -> std::io::Result<()> {
     let mut line_iterator = BufReader::new(file).lines().into_iter();
 
     let pattern = Regex::new("[Ss][Ee][Nn][Dd][Ii][Nn][Gg]\\s+\\D*(\\d+)\\s+\\D*[Ss][Ee][Nn][Tt]\\s+\\D*(\\d+)").unwrap();
+
+    println!("This console tool reads logs file (usually named \"swadlwrapper.log\" and interprets output to display upload progress.");
+    println!("When tool sees line with 'Sending' and 'Send' words with numbers after EACH of them, it interprets this line as a helthy output.");
+    println!("which indicates that upload is progressing as intended.");
+    println!("If such pattern is not found - tool warns user with message: \"This log line indicates no data transfer in this moment!\"");
 
     loop {
         loop {
@@ -43,7 +48,7 @@ fn main() -> std::io::Result<()> {
             let captures = pattern.captures(&line);
 
             let captures = if captures.is_none() {
-                println!("{}, Logs from file \"{}\": {}", "No data sent yet".red(), file_name, line.yellow());
+                println!("{} Logs from file \"{}\": {}", "This log line indicates no data transfer in this moment!".red(), file_name, line.yellow());
 
                 continue;
             } else {
@@ -60,9 +65,9 @@ fn main() -> std::io::Result<()> {
             let sent_bytes_number_as_str = format!("{}", captures.get(2).unwrap().as_str().parse::<u64>().unwrap()).blue();
             let percentage = format!("{:.1}", ((sent_bytes_number as f32 / pack_size as f32) * 100 as f32)).green();
 
-            let current_time = Local::now().format("%H:%M:%S").to_string();
+            let current_time = Local::now().format("%H:%M:%S").to_string().cyan();
 
-            println!("{} :: Uploaded {}kB out of {}kB. Uploaded {:.1}%", current_time, sent_bytes_number_as_str, pack_size_as_str, percentage);
+            println!("{} :: Uploaded {:>10} / {}[kB] == {:>10}%", current_time, sent_bytes_number_as_str, pack_size_as_str, percentage);
         }
 
         thread::sleep(Duration::from_millis(200));
